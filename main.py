3e5178aa0e7faa3aa550cc5ec306affcd12cdf76
SCPassion <bernard.cryptohk@gmail.com>
2025-06-23 08:25:44 +0200

    Refactor main.py to include character count analysis and add restruct_dict_to_array function in stats.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from stats import count_words, count_unique_words
+from stats import count_words, count_unique_words, restruct_dict_to_array
 
 def main():
   file_path = "books/frankenstein.txt"
   book_text = get_book_text(file_path)
   word_count = count_words(book_text)
   unique_words = count_unique_words(book_text)
-  print(unique_words)
+  words_count_array = restruct_dict_to_array(unique_words)
+
+  print("============ BOOKBOT ============")
+  print("Analyzing book found at books/frankenstein.txt...")
+  print("----------- Word Count ----------")
+  print(f"Found {word_count} total words")
+  print("--------- Character Count -------")
+  for dict in words_count_array:
+    if dict["char"].isalpha():
+      print(f"{dict["char"]}: {dict["num"]}")
 
 def get_book_text(file_path):
   file_content = ""
--- stats.py
+++ stats.py
@@ -13,3 +13,14 @@ def count_unique_words(text):
       mapping_dict[char] = 1
 
   return mapping_dict
+
+def restruct_dict_to_array(mapping_dict):
+  new_dict_array = []
+  for key in mapping_dict:
+    new_dict_array.append({"char": key, "num": mapping_dict[key]})
+
+  new_dict_array.sort(reverse=True, key=sort_on)
+  return new_dict_array
+
+def sort_on(items):
+  return items["num"]
